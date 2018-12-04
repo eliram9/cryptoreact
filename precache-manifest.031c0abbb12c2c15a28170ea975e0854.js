90491ac2428221569a5892c8188def9e39e8f36a
Malachi <emalachi@deloitte.com>
2018-12-03 20:11:13 -0500

    Updates

---
+++ precache-manifest.031c0abbb12c2c15a28170ea975e0854.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb139f520d2696706f0309f2ff5c5168",
+    "url": "/cryptoreact/static/media/xrp.eb139f52.png"
+  },
+  {
+    "revision": "5b106ce14c1027b3693e",
+    "url": "/cryptoreact/static/css/main.ae170686.chunk.css"
+  },
+  {
+    "revision": "2c80947bbb5483014cb2bc3ee31be9b8",
+    "url": "/cryptoreact/static/media/bc.2c80947b.jpeg"
+  },
+  {
+    "revision": "d918f49c598b7ce2c05d",
+    "url": "/cryptoreact/static/js/1.d918f49c.chunk.js"
+  },
+  {
+    "revision": "22ee13b2a88b93b5f155",
+    "url": "/cryptoreact/static/js/runtime~main.22ee13b2.js"
+  },
+  {
+    "revision": "2e34a9e915969a2d077f02566a15a634",
+    "url": "/cryptoreact/static/media/btc.2e34a9e9.png"
+  },
+  {
+    "revision": "5b106ce14c1027b3693e",
+    "url": "/cryptoreact/static/js/main.5b106ce1.chunk.js"
+  },
+  {
+    "revision": "513f17ddfc843489b50f6ab3d6de175b",
+    "url": "/cryptoreact/static/media/bch.513f17dd.png"
+  },
+  {
+    "revision": "c1d49e0e606aef9c072a6f7462fa681e",
+    "url": "/cryptoreact/static/media/etc.c1d49e0e.png"
+  },
+  {
+    "revision": "6db5b8a245f07a5066711f5a396e7d5d",
+    "url": "/cryptoreact/static/media/xlm.6db5b8a2.png"
+  },
+  {
+    "revision": "d918f49c598b7ce2c05d",
+    "url": "/cryptoreact/static/css/1.7f6e1ec3.chunk.css"
+  },
+  {
+    "revision": "26bc3196e50d63ebc3513a9d019c747d",
+    "url": "/cryptoreact/index.html"
+  }
+];
